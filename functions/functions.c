c81aa3853026fd8d3166e2dc3b7fbed2a4d1a5a2
HenriqueKutner <kutner.cordeiro@gmail.com>
2025-01-27 13:55:33 -0300

    function

--- functions/functions.c
+++ functions/functions.c
@@ -15,12 +15,24 @@ Crie uma funcção que faça soma e subtração ao mesmo tempo.
 int *som = endereço de memória de soma 
 int *som tem um endereço de memória ex. A100 e também um valor, nesse caso o endereço de ex. C A104
 Como eu leio int *som? Assim: Para que valor inteiro esse ponteiro está apontando?
+
+Para que existe ponteiros? Eficiência. Imagine uma função que recebe dois valores, eu posso 
+fazer de duas maneiras, passar os argumentos e receber na função como variáveis normais, a função cria
+uma cópia de cada parametro em uma nova caixa de memória e após o término da função é liberado.asm
+Por outra via existe os ponteiros, em que posso passar os argumentos por referencia e a função não precisa
+criar uma nova cópia.
 */
 
 void soma_e_subtracao(int x, int y, int *som, int *sub){
-
   *som = x + y;
   *sub = x - y;
+}
+
+void troca(int *x, int *y){
+  int aux = *x;
+  *x = *y;
+  *y = aux;
+
 }
 
 int main()
@@ -31,5 +43,6 @@ int main()
   int subtracao;
   soma_e_subtracao(a, b, &soma, &subtracao);
   printf("Soma = %d e a subtracao é %d", soma, subtracao);
+  troca(&a, &b);
 
 }

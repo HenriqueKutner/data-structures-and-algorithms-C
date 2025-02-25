d7726a9072fae8e7fd865888cf835bbbb9bd5063
HenriqueKutner <kutner.cordeiro@gmail.com>
2025-02-25 11:19:58 -0300

    more exercices

--- static_and_dynamic_allocation/exercices/06.c
+++ static_and_dynamic_allocation/exercices/06.c
@@ -6,19 +6,27 @@ setando-o como NULL após a desalocaçao
 #include <stdio.h>
 #include <stdlib.h>
 
-void desaloca_vetor(int v[]){
+void desaloca_vetor(int **v){
+    // Liberando a heap
     free(*v);
+    // Inserindo null no valor da stack
+    *v = NULL;
 }
 
 int main() {
     int *v = (int*) calloc(4, sizeof(int));
+    printf("Endereço de v (stack) = %p, Valor de v = %p, Conteúdo de v = %d\n", &v, v, *v);
     for(int i = 0; i < 4; i++){
-        printf("%d\n", v[i]);
+        printf("Endereço = %p, Valor = %d\n", &v[i], v[i]);
     }
-    desaloca_vetor(v);
+
+    desaloca_vetor(&v);
     puts("***********************************");
     for(int i = 0; i < 4; i++){
-        printf("%d\n", v[i]);
+        printf("Endereço = %p, Valor = %d\n", &v[i], v[i]);
     }
+
+    
+    
     return 0;
 }

098899c69e57018c8ac0ee02a4e025f24d0837ab
HenriqueKutner <kutner.cordeiro@gmail.com>
2025-02-27 10:47:19 -0300

    matriz

---
+++ matrizes/matriz_estatica.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n_rows = 2;
+    int n_cols = 3;
+    int m[2][3] = {{0, 1, 2}, {3, 4, 5}};
+
+    for (int i = 0; i < n_rows; i++) {
+        for(int j = 0; j < n_cols; j++){
+
+        }
+    }
+    return 0;
+}

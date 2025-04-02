1215f238d712d7e26e191431c1476d062d255fca
HenriqueKutner <kutner.cordeiro@gmail.com>
2025-04-02 11:29:00 -0300

    ram

--- ram_memory.c
+++ ram_memory.c
@@ -13,17 +13,27 @@ A RAM sempre é endereçada por byte (1 byte por endereço).
 1 byte = 8 bits.
 Se um programa armazenar um long long (64 bits), 
 ele usará 8 bytes consecutivos.
+
+hexadecimal = 0x7ffd423fd274 - em sistemas x64 endereços na stack começam em 0x7
 */
 
 #include <stdio.h>
+
 int main() {
     int a = 4;
     int b = 5;
     int c = 5;
     int *d;
-    printf("%p\n", &a);
+    printf("%p\n", (void*) &a);
     printf("%p\n", &b);
     printf("%p\n", &c);
     printf("%p\n", &d);
+
+
+    // Imprimindo tamanhos ocupados na memória ram
+    printf("Tamanho de int: %zu bytes\n", sizeof(&a)); // 8 bytes
+    printf("Tamanho de int: %zu bytes\n", sizeof(a)); // 4 bytes
+    printf("Tamanho de int: %zu bytes\n", sizeof(d)); // 8 bytes
+    printf("Tamanho de int: %zu bytes\n", sizeof(*d)); // 4 bytes
     return 0;
 }

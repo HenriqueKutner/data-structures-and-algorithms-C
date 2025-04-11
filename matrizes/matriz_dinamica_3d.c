3215465cc12a2e6221e74aa6e2bf8adbb95d1d00
HenriqueKutner <kutner.cordeiro@gmail.com>
2025-04-11 10:31:55 -0300

    printing 3d

--- matrizes/matriz_dinamica_3d.c
+++ matrizes/matriz_dinamica_3d.c
@@ -22,15 +22,31 @@ int main() {
     // md = 0x55f8910c82a0
     printf("%p, %p\n", &md, md);
     /*
-    Para cada slice vou alocar duas linhas e para cada linha vou alocar 3 colunas
-    
-    Primeiro: Alocar duas linhas na heap que apontarao para para as colunas
+    Para cada slice vou alocar duas linhas e para cada linha vou alocar tres colunas.
+    Primeiro: Para o slice[0] vou alocar a primeira linha.
+    Segundo: Para o slice[1] vou alocar a segunda linha.
+    Terceiro: Para o slice[0] linha[0] vou alocar tres colunas
+    Quarto: Para o slice[0] linha[1] vou alocar tres colunas
+    Assim por diante...
     */
 
+    int value = 0;
     for(int i = 0; i < N_SLICES; i++) {
         md[i] = (int**) calloc(N_ROWS, sizeof(int*));
         for(int j = 0; j < N_ROWS; j++) {
             md[i][j] = (int*) calloc(N_COLS, sizeof(int));
+            for(int k = 0; k < N_COLS; k++) {
+                md[i][j][k] = value;
+                value++;
+            }
+        }
+    }
+
+    for(int i = 0; i < N_SLICES; i++) {
+        for(int j = 0; j < N_ROWS; j++) {
+            for(int k = 0; k < N_COLS; k++) {
+                printf("md[%d][%d][%d] = %d\n", i, j, k, md[i][j][k]);
+            }
         }
     }
 }
